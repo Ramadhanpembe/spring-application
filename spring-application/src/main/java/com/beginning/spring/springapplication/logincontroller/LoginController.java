025982e36167895e40d3f43fb7cd6de991840eca
ramadhanpembe <ramadhanpembe9@gmail.com>
2021-04-29 21:35:59 +0300

    Please help me brother

---
+++ spring-application/src/main/java/com/beginning/spring/springapplication/logincontroller/LoginController.java
@@ -0,0 +1,16 @@
+package com.beginning.spring.springapplication.logincontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	
+	@RequestMapping("/login")
+	public String loginMessage() {
+		
+		return "login";
+	}
+
+}
---
+++ spring-application/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>This is a JSP PROGRAM!!!</title>
+</head>
+<body>
+<h1>Hi! you know what!!!!</h1>
+<h1>I simply don't give up</h1>
+</body>
+</html>
